bcd2486d4e510c30fbd30ac2b2ba35c7e3b1997b
bharathraj1908 <bharathkrishnan1908@gmail.com>
2026-09-16 11:44:05 +0530

    updated project

---
+++ JS/Day4/Array.js
@@ -0,0 +1,43 @@
+let students = ["Arun", "Bala", "Kumar", "Rahul", "Vijay"];
+
+console.log(students[0]); // First student
+console.log(students[2]); // Third student
+console.log(students[4]); // Last student
+
+
+let prices = [1500, 2500, 3500, 4500, 5500];
+
+let first = prices[0];
+let last = prices[4];
+
+let total = first + last;
+
+console.log(total);
+
+let arr = ["HTML", "CSS", "JavaScript", "React", "Node"];
+
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[arr.length - 1]);
+
+
+let data = [
+    ["Arun", 80],
+    ["Bala", 75],
+    ["Kumar", 90]
+];
+
+console.log(data[0][0]);
+console.log(data[0][1]);
+console.log(data[1][0]);
+console.log(data[2][1]);
+
+
+let numbers = [10, 20, 30, 40, 50];
+
+let second = numbers[1];
+let secondLast = numbers[numbers.length - 2];
+
+let answer = second + secondLast;
+
+console.log(answer);
